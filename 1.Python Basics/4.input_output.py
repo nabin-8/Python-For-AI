24fc248107d0673d90a7046dc73a1386588a5196
Nabin Acharya <nabinacharya740@gmail.com>
2024-03-05 12:44:01 +0545

    inputoutput

---
+++ 1.Python Basics/4.input_output.py
@@ -0,0 +1,51 @@
+# OUTPUT
+name="python"
+# print(name)
+
+
+# Input
+# x=int(input("Enter a number: "))
+# age=input("Enter your age: ")
+# print(x)
+# print(age)
+
+# print("Hello My name is :",name, "and my age is: ",age)
+
+'''
+    python
+    Enter your age: 28
+    28
+    Hello My name is : python and my age is:  28
+'''
+
+'''
+    Exercise 1:
+    Write a Python program that prompts the user to enter their name and age. The program should then display a welcome message using these inpute.
+'''
+
+# UserName=input("Enter Your Name: ")
+# age=input("Enter Your age: ")
+# print("Hi "+UserName+". You are "+age+" years old.")
+# Supported for the formatted string
+# print(f"Hi {UserName} You are {age} years old.")
+
+'''
+    Enter Your Name: Nabin
+    Enter Your age: 55
+    Hi Nabin. You are 55 years old.
+    Hi Nabin You are 55 years old.
+'''
+
+# comma saperated valie
+x=2
+y=3
+z=4
+print(x, y, z, sep=',')
+print(x, y, z, sep='$')
+# 2,3,4
+# 2$3$4
+
+print('Python')
+print('Python', end=' ')
+print('World', end=' ')
+
