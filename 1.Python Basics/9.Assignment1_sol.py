e9b0ac8fe1d065871b2bda662b300b16891af8e8
Nabin Acharya <nabinacharya740@gmail.com>
2024-03-14 17:16:17 +0545

    pyassignments

---
+++ 1.Python Basics/9.Assignment1_sol.py
@@ -0,0 +1,26 @@
+# Qn1
+# user1=int(input("Enter Number1: "))
+# user2=int(input("Enter Number2: "))
+# user3=int(input("Enter Number3: "))
+
+# if (user1>user2 and user1>user3):
+#     print("User 1 is greater")
+# elif (user2>user3):
+#     print("User 2 is greater")
+# else:
+#     print("User 3 is greater")
+
+
+# Qno2
+# year=int(input("Enter Year: "))
+# # if(year/0):
+
+# print((year/100))
+# print((year/100)==0 and not(year/400))
+
+
+# Qno3
+for x in range(5):
+    print(x)
+    for y in range(5):
+        print(y)
