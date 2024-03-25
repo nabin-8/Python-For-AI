8227772cf6c5bfc5ba454983961d03425e291665
Nabin Acharya <nabinacharya740@gmail.com>
2024-03-25 18:50:54 +0545

    multipleinit

---
+++ 6.Oops Concepts/5.mutliple_init.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self,name, age=99, hobby="cricket"):
+        self.name=name
+        self.age=age
+        self.hobby=hobby
+
+
+
+per1=Person("Nabin")
+per2=Person("Nabin",56)
+per3=Person("Nabin",65,"football")
+
+print(per1.hobby)
+print(per3.hobby)
+
+'''
+
+'''
