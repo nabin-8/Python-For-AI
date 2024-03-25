05a7b417be2e1f619061eb7fbd66f4c067b77610
Nabin Acharya <nabinacharya740@gmail.com>
2024-03-25 17:25:26 +0545

    classandobject

---
+++ 6.Oops Concepts/1.Classes_and_Objects.py
@@ -0,0 +1,20 @@
+# class is a keyword
+# Person is a name of class
+# Normally class starts with capital letter
+# All the rules of variable is apply to define a class
+class Person:
+    pass
+    # we cannot fill empty so class is used
+    # class body
+
+
+obj=Person()
+# obj is the object
+# Blueprint of Person class
+
+print(type(obj))
+# Output: <class '__main__.Person'>
+
+print(obj)
+# Address of object
+# <__main__.Person object at 0x000002ED31ED59A0>
